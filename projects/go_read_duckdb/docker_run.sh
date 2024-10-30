f223afa53d1cab90e009fac1a0ead10bb18dfce4
Ren Wang <iamwrm@gmail.com>
2024-10-30 13:39:01 +0000

    fix

--- projects/go_read_duckdb/docker_run.sh
+++ projects/go_read_duckdb/docker_run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ueo pipefail
 
+cd `dirname $0`
+
 # Check if Docker is installed
 if ! command -v docker &> /dev/null; then
     echo "Docker is not installed. Please install Docker first."
