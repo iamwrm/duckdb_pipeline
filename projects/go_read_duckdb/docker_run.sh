9f339b90a641fda0381890240d579e36f203a5d8
Ren Wang <iamwrm@gmail.com>
2024-10-31 11:51:20 +0800

    fix

--- projects/go_read_duckdb/docker_run.sh
+++ projects/go_read_duckdb/docker_run.sh
@@ -10,6 +10,10 @@ if ! command -v docker &> /dev/null; then
 fi
 
 echo "Create CSV file..."
+
+# Use buildx
+export DOCKER_BUILDKIT=1
+
 docker build -t csv-reader-py . -f python.Dockerfile
 echo "Generating CSV file..."
 docker run --rm -v "$(pwd):/app" csv-reader-py python3 create_data.py
@@ -17,9 +21,11 @@ docker run --rm -v "$(pwd):/app" csv-reader-py python3 create_data.py
 echo "Run golang in ubi9"
 DOCKER_NAME=csv-reader-go-ubi9
 docker build -t $DOCKER_NAME . -f go.ubi9.Dockerfile
-docker run --rm -v "$(pwd):/data/" -w /data $DOCKER_NAME /app/read_data 
+docker run --rm -v "$(pwd):/data/" -w /data $DOCKER_NAME \
+    /bin/bash -c "CGO_ENABLED=1 GOOS=linux go build -o read_data && ./read_data"
 
 echo "Run golang in golang"
 DOCKER_NAME=csv-reader-go-golang
 docker build -t $DOCKER_NAME . -f go.golang.Dockerfile
-docker run --rm -v "$(pwd):/data/" -w /data $DOCKER_NAME /app/read_data 
+docker run --rm -v "$(pwd):/data/" -w /data $DOCKER_NAME \
+    /bin/bash -c "CGO_ENABLED=1 GOOS=linux go build -o read_data && ./read_data"
--- projects/go_read_duckdb/go.golang.Dockerfile
+++ projects/go_read_duckdb/go.golang.Dockerfile
@@ -6,9 +6,5 @@ WORKDIR /app
 COPY go.mod go.sum ./
 RUN go mod download
 
-COPY read_data.go .
-# Build with static linking
-RUN CGO_ENABLED=1 GOOS=linux go build -o read_data
 
-
-CMD ["/app/read_data"]
+CMD ["/bin/bash"]
